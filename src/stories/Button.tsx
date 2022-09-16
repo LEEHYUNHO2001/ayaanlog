658d6c7d9c2a177971552d6686348d7bfd1a93c8
이현호 <dlgusgh2001@naver.com>
2022-09-16 15:59:12 +0900

    :recycle: storybook button emotion

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,49 +1,65 @@
-import './button.css';
-
-import React from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
   size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
+interface ButtonStyled {
+  primary: boolean;
+  size: string;
+  backgroundColor?: string;
+}
+
 export const Button = ({
   primary = false,
   size = 'medium',
   backgroundColor,
   label,
   ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+}: ButtonProps) => (
+  <Btn type="button" primary={primary} size={size} backgroundColor={backgroundColor} {...props}>
+    {label}
+  </Btn>
+);
+
+const Btn = styled.button<ButtonStyled>`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+
+  ${({ backgroundColor }) => `background-color: ${backgroundColor};`}
+  ${({ primary }) => (primary ? Primary : Original)}
+  ${({ size }) => size === 'small' && Small}
+  ${({ size }) => size === 'medium' && Medium}
+  ${({ size }) => size === 'large' && Large}
+`;
+const Small = css`
+  font-size: 12px;
+  padding: 10px 16px;
+`;
+const Medium = css`
+  font-size: 14px;
+  padding: 11px 20px;
+`;
+const Large = css`
+  font-size: 16px;
+  padding: 12px 24px;
+`;
+const Primary = css`
+  color: white;
+  background-color: #1ea7fd;
+`;
+const Original = css`
+  color: #333;
+  background-color: transparent;
+  box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+`;
